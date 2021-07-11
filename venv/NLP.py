9b1f51928eba5aac83188cf0c46c16b534ab2d65
LeeMyeonggyu <si44440@naver.com>
2021-07-11 15:58:36 +0900

    first

--- venv/NLP.py
+++ venv/NLP.py
@@ -7,13 +7,24 @@ window = app.top_window()
 window.set_focus()
 
 # print(window.print_control_identifiers())
-
-wins = window.child_window(title_re='.*아이유.*', found_index=0)
-wins.click_input()
-
-
-all_del='+{UP}{DEL}'
-s='hihi'
-text = window.child_window(control_type='ComboBox',auto_id='search')
-keyboard.send_keys(all_del)
-keyboard.send_keys(s)
+# for i in range(100):
+#     wins = window.child_window(auto_id='res').child_window(control_type='Hyperlink',found_index=i)
+#     print(wins.print_control_identifiers())
+#     if i == 3:
+#         wins.click_input()
+# for i in range(5):
+#
+#     wins = window.child_window(auto_id='video-title',found_index=i)
+#     print(wins.print_control_identifiers())
+#     if i == 4:
+#         wins.click_input()
+# wins = window.child_window(title_re='.*아이유.*', found_index=0)
+# wins.click_input()
+#
+#
+# all_del='+{UP}{DEL}'
+# s='hihi'
+# text = window.child_window(control_type='ComboBox',auto_id='search')
+# keyboard.send_keys(all_del)
+# keyboard.send_keys(s)
+# #
